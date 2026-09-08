87d3008438147bffce5de3b2faf84cb89da64f98
Yotam Barak <yotambarak6@gmail.com>
2026-09-08 23:56:12 +0300

    renaming branch to CLI

--- crates/surge-cli/src/main.rs
+++ crates/surge-cli/src/main.rs
@@ -1,22 +1,13 @@
 mod config;
-mod vu;
 
 use std::sync::Arc;
 
 use clap::Parser;
 use config::Config;
-use mlua::Result as LuaResult;
-
-use crate::vu::Vu;
 
 #[tokio::main]
-async fn main() -> LuaResult<()> {
+async fn main() {
     let config = Arc::new(Config::parse());
 
-    let my_vu = Vu::new(config.clone());
-
-    my_vu.initialize();
-    my_vu.mainloop().await;
-
-    Ok(())
+    dbg!(config);
 }
