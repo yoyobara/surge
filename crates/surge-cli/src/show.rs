41e5d4aaa2a05fba43b32f2b1f94c0fd3d37f5be
Yotam Barak <yotambarak6@gmail.com>
2026-09-13 16:25:44 +0300

    show command

---
+++ crates/surge-cli/src/show.rs
@@ -0,0 +1,40 @@
+use std::{fs, path::Path};
+
+use petgraph::{Graph, graph::NodeIndex};
+use ptree::print_tree;
+use std::io::Result as IoResult;
+
+use crate::config::ShowArgs;
+
+type TestsGraph = Graph<String, ()>;
+
+fn build_dir(graph: &mut TestsGraph, current_path: &Path, parent: NodeIndex) -> IoResult<()> {
+    for entry in fs::read_dir(current_path)? {
+        let entry = entry?;
+        let path = entry.path();
+        let name = entry.file_name().to_string_lossy().to_string();
+
+        if entry.file_type()?.is_dir() {
+            let node = graph.add_node(name);
+            build_dir(graph, &path, node)?;
+            if graph.neighbors(node).next().is_some() {
+                graph.add_edge(parent, node, ());
+            }
+        } else if name.ends_with(".spec.lua") {
+            let node = graph.add_node(name);
+            graph.add_edge(parent, node, ());
+        }
+    }
+
+    Ok(())
+}
+
+pub fn handle_show(args: &ShowArgs) -> IoResult<()> {
+    let mut graph = TestsGraph::new();
+    let root = graph.add_node(".".to_string());
+
+    build_dir(&mut graph, Path::new("."), root)?;
+    print_tree(&(&graph, root))?;
+
+    Ok(())
+}
